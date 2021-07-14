graalvm/labs-openjdk-15

added CI config

defs.jsonnet
@@ -1,0 +1,6 @@
+{
+    linux_docker_image:: "<placeholder for overlay>",
+    labsjdk_builder_url:: "<placeholder for overlay>",
+    jib_server:: "<placeholder for overlay>",
+    jib_server_mirrors:: "<placeholder for overlay>",
+}

openjdk/riscv-port

defs.jsonnet
@@ -1,0 +1,8 @@
+{
+    linux_docker_image_aarch64:: "<placeholder for overlay>",
+    linux_docker_image_amd64:: "<placeholder for overlay>",
+    linux_docker_image_amd64_musl:: "<placeholder for overlay>",
+    labsjdk_builder_url:: "<placeholder for overlay>",
+    jib_server:: "<placeholder for overlay>",
+    jib_server_mirrors:: "<placeholder for overlay>",
+}