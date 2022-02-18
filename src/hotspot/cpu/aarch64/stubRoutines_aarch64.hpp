82c76aa6eca6e0818a859a71a2312da0e694c470
Yudi Zheng <yudi.zheng@oracle.com>
2022-02-18 09:19:51 +0100

    Export _sha3_implCompress, _md5_implCompress and aarch64::_has_negatives stubs to JVMCI compiler.

--- src/hotspot/cpu/aarch64/stubRoutines_aarch64.hpp
+++ src/hotspot/cpu/aarch64/stubRoutines_aarch64.hpp
@@ -58,8 +58,6 @@ class aarch64 {
 
   static address _zero_blocks;
 
-  static address _has_negatives;
-  static address _has_negatives_long;
   static address _large_array_equals;
   static address _compare_long_string_LL;
   static address _compare_long_string_LU;
@@ -75,6 +73,9 @@ class aarch64 {
   static bool _completed;
 
  public:
+
+  static address _has_negatives;
+  static address _has_negatives_long;
 
   static address get_previous_sp_entry()
   {
